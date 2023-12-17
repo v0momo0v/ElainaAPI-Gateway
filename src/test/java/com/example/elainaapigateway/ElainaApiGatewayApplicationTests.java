72c2f7c4a4a093256baf37ace8937cb91ba1fe89
Gavin <1524018075@qq.com>
2023-12-18 00:00:50 +0800

    API网关

---
+++ src/test/java/com/example/elainaapigateway/ElainaApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elainaapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElainaApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
